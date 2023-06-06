fbf584fbbfa7d14dba5d06da4b2121df3bb5dfaf
Felix Hahn <info@hahn-felix.de>
2023-06-06 23:18:46 +0200

    Move logic into widget

--- widgets/SwitchButton.php
+++ widgets/SwitchButton.php
@@ -3,14 +3,34 @@
 namespace humhub\modules\darkMode\widgets;
 
 use humhub\components\Widget;
+use humhub\modules\ui\icon\widgets\Icon;
 
 /**
  * Button for switching between dark and light mode
  */
 class SwitchButton extends Widget
 {
+    public $mode = 'light';
+    
+    public $icon = '';
+    
+    public function init()
+    {
+        if(!empty($_COOKIE['theme'])) {
+	        $this->mode = $_COOKIE['theme'];
+        }
+        if($this->mode == 'dark') {
+            $this->icon = Icon::get('sun-o');
+        } else {
+            $this->icon= Icon::get('moon-o');
+        }
+    }
+    
     public function run()
     {
-        return $this->render('switchButton');
+        return $this->render('switchButton', [
+            'mode' => $this->mode,
+            'icon' => $this->icon,
+        ]);
     }
 }
--- widgets/views/switchButton.php
+++ widgets/views/switchButton.php
@@ -1,22 +1,12 @@
 <?php
 
-use humhub\modules\ui\icon\widgets\Icon;
 use humhub\modules\darkMode\assets\DarkModeAsset;
 use humhub\modules\darkMode\assets\DarkStyleAsset;
 
 DarkModeAsset::register($this);
 
-$mode = 'light';
-
-if(!empty($_COOKIE['theme'])) {
-	$mode = $_COOKIE['theme'];
-}
-
 if ($mode == 'dark') {
     DarkStyleAsset::register($this);
-    $icon = Icon::get('sun-o');
-} else {
-    $icon = Icon::get('moon-o');
 }
 ?>
 <div class="btn-group">
