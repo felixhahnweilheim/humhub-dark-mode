774271c53b70463e072512c36270567a36ccd8b4
Felix Hahn <info@hahn-felix.de>
2023-06-06 23:29:46 +0200

    Enh: SameSite cookie attribute

--- resources/js/darkmode.js
+++ resources/js/darkmode.js
@@ -19,5 +19,5 @@ function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/" + ";SameSite=Lax";
 }
