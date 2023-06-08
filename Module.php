20576521a12483d31c040f46c56ea77915a6d344
Felix Hahn <info@hahn-felix.de>
2023-06-08 22:36:11 +0200

    add icon, screenshots

--- Module.php
+++ Module.php
@@ -8,8 +8,12 @@ class Module extends \humhub\components\Module {
 
     public $resourcesPath = 'resources';
 
+    // Translatable Module Name
+    public function getName() {
+        return Yii::t('DarkModeModule.admin', 'Dark Mode for HumHub');
+    }
     // Translatable Module Description
     public function getDescription() {
-        return Yii::t('DarkModeModule.admin', 'Dark Mode for HumHub');
+        return Yii::t('DarkModeModule.admin', 'Add a Dark Mode to HumHub - based on the community theme');
     }
 }
