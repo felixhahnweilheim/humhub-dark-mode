51a89472b6a5f1165902f84f838b557b662427ff
Felix Hahn <info@hahn-felix.de>
2023-07-12 16:26:17 +0200

    prepare selectable theme (#3)
    * Create Config.php
    
    * Update DarkStyleAsset.php
    
    * Update DarkStyleAsset.php
    
    * Update DarkStyleAsset.php
    
    * Update DarkStyleAsset.php typo
    
    * Update DarkStyleAsset.php
    
    * Update DarkStyleAsset.php
    
    * Update DarkStyleAsset.php

--- assets/DarkStyleAsset.php
+++ assets/DarkStyleAsset.php
@@ -2,6 +2,8 @@
 
 namespace humhub\modules\darkMode\assets;
 
+use humhub\modules\ui\view\helpers\ThemeHelper;
+use Yii;
 use yii\web\AssetBundle;
 
 class DarkStyleAsset extends AssetBundle
@@ -9,10 +11,18 @@ class DarkStyleAsset extends AssetBundle
     public $publishOptions = [
         'forceCopy' => false
     ];
-    
+
     public $sourcePath = '@dark-mode/themes/DarkHumHub';
     
     public $css = [
         ['css/theme.css', 'media' => 'screen and (prefers-color-scheme: dark)'],
     ];
+    
+    public function init()
+    {
+        $themeName = Yii::$app->getModule('dark-mode')->settings->get('theme', 'DarkHumHub');
+        $theme = ThemeHelper::getThemeByName($themeName);
+        
+        $this->sourcePath = $theme->basePath;
+    }
 }
