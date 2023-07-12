483279ab8c374a0675dfcd02d963e9f8147a1b36
Felix Hahn <info@hahn-felix.de>
2023-07-12 16:56:56 +0200

    Update index.php

--- views/config/index.php
+++ views/config/index.php
@@ -5,16 +5,18 @@ use humhub\modules\ui\form\widgets\ActiveForm;
 use yii\helpers\Html;
 
 ?>
-<div class="panel-body">
-    <?php $form = ActiveForm::begin(['id' => 'configure-form']);?>
 
-        <?= $form->field($model, 'theme')->dropdownList($model->getThemes());
-        ?>
+<div class="panel pabel-default">
+    <div class="panel-body">
+        <?php $form = ActiveForm::begin(['id' => 'configure-form']);?>
 
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']) ?>
+            <?= $form->field($model, 'theme')->dropdownList($model->getThemes());
+            ?>
+
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
     </div>
-
-    <?php ActiveForm::end(); ?>
-
 </div>
