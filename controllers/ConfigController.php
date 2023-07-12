e6de4aae0b510151adef33f163eb52ebfe74ad1c
Felix Hahn <info@hahn-felix.de>
2023-07-12 16:44:38 +0200

    Create ConfigController.php

---
+++ controllers/ConfigController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace humhub\modules\darkMode\controllers;
+
+use humhub\modules\darkMode\models\Config;
+use Yii;
+
+class ConfigController extends \humhub\modules\admin\components\Controller
+{
+    public function actionIndex()
+    {
+        $form = new Config();
+
+        if ($form->load(Yii::$app->request->post()) && $form->save()) {
+            $this->view->saved();
+        }
+
+        return $this->render('index', ['model' => $form]);
+    }
+}
