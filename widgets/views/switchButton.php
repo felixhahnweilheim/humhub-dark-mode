128a70bdfde1cc207618a3418fb10de9eecd279d
Felix Hahn <info@hahn-felix.de>
2023-07-12 16:28:53 +0200

    Update switchButton.php

--- widgets/views/switchButton.php
+++ widgets/views/switchButton.php
@@ -1,13 +1,5 @@
 <?php
 
-use humhub\modules\darkMode\assets\DarkModeAsset;
 use humhub\modules\darkMode\assets\DarkStyleAsset;
 
 DarkStyleAsset::register($this);
-
-?>
-<div class="btn-group">
-    <a href="#" id="switch-button" class="currently-<?= $mode ?>">
-        <?= $icon ?>
-    </a>
-</div>
