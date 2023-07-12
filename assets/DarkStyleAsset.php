b41571f0332d75631ccc520630f76b786087b586
Felix Hahn <info@hahn-felix.de>
2023-07-12 16:00:37 +0200

    Update DarkStyleAsset.php

--- assets/DarkStyleAsset.php
+++ assets/DarkStyleAsset.php
@@ -9,8 +9,11 @@ class DarkStyleAsset extends AssetBundle
     public $publishOptions = [
         'forceCopy' => false
     ];
+
+    $themeName = Yii::app->getModule('dark-mode')->get('theme', 'DarkHumHub');
+    $theme = ThemeHelper::getThemeByName($themeName);
     
-    public $sourcePath = '@dark-mode/themes/DarkHumHub';
+    public $sourcePath = $theme->basePath;
     
     public $css = [
         'css/theme.css',
