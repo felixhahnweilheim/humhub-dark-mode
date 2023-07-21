0a3fc9bd63a7ef9056ec190f26a4d69005cd485b
Felix Hahn <info@hahn-felix.de>
2023-07-21 16:58:38 +0200

    always show moon

--- widgets/SwitchButton.php
+++ widgets/SwitchButton.php
@@ -16,20 +16,12 @@ class SwitchButton extends Widget
     
     public function init()
     {
-        if(!empty($_COOKIE['theme'])) {
-	        $this->mode = $_COOKIE['theme'];
-        }
-        if($this->mode == 'dark') {
-            $this->icon = Icon::get('sun-o');
-        } else {
-            $this->icon= Icon::get('moon-o');
-        }
+        $this->icon= Icon::get('moon-o');
     }
     
     public function run()
     {
         return $this->render('switchButton', [
-            'mode' => $this->mode,
             'icon' => $this->icon,
         ]);
     }
--- widgets/views/switchButton.php
+++ widgets/views/switchButton.php
@@ -3,12 +3,9 @@
 use humhub\modules\darkMode\assets\DarkStyleAsset;
 use yii\helpers\Url;
 
-if ($mode == 'dark') {
-    DarkStyleAsset::register($this);
-}
 ?>
 <div class="btn-group">
-    <a id="dark-mode-button" href="#" data-action-click="ui.modal.load" data-action-click-url="<?= Url::toRoute('/dark-mode/user/') ?>" class="currently-<?= $mode ?>">
+    <a id="dark-mode-button" href="#" data-action-click="ui.modal.load" data-action-click-url="<?= Url::toRoute('/dark-mode/user/') ?>">
         <?= $icon ?>
     </a>
 </div>
