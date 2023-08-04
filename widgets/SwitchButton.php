642728bff8d0f3b21916f60d453a274bf31891dc
Felix Hahn <info@hahn-felix.de>
2023-08-04 12:13:36 +0200

    move logic from view to widget

--- widgets/SwitchButton.php
+++ widgets/SwitchButton.php
@@ -4,6 +4,7 @@ namespace humhub\modules\darkMode\widgets;
 
 use humhub\components\Widget;
 use humhub\modules\ui\icon\widgets\Icon;
+use yii\helpers\Url;
 
 /**
  * Button for switching between dark and light mode
@@ -12,16 +13,20 @@ class SwitchButton extends Widget
 {
     public $icon = '';
     
+    public $url;
+    
     public function init()
     {
         // Always use moon icon
         $this->icon = Icon::get('moon-o');
+        $this->url = Url::toRoute('/dark-mode/user/')
     }
     
     public function run()
     {
         return $this->render('switchButton', [
             'icon' => $this->icon,
+            'url' => $this->url,
         ]);
     }
 }
