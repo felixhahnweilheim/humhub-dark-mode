86c0b5fb2af6d37eae25f39ff608bebde7ae4843
Felix Hahn <info@hahn-felix.de>
2023-08-18 23:37:04 +0200

    add build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Recompile CSS files
 cd resources/DarkHumHub/less
 lessc ./build.less ../css/theme.css --clean-css="--s1 --advanced" --source-map=../css/theme.css.map
 cd ../../DarkEnterprise/less
 lessc ./build.less ../css/theme.css --clean-css="--s1 --advanced" --source-map=../css/theme.css.map
 cd ../../..
+
+# Update message files
+cd ../../ php yii message/extract-module dark-mode
+cd modules/dark-mode
