b4d4d50ac3e66295f098ea26c05ffac7392d46de
Felix Hahn <info@hahn-felix.de>
2023-08-19 11:15:25 +0200

    re-add CSS variables after css-color-extractor

--- build.sh
+++ build.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-# Recompile CSS files and extract colors
+# 1) Recompile CSS files and extract colors
+# 1a) DARKHUMHUB
 cd resources/DarkHumHub
-lessc less/build.less css/theme.css --clean-css="--s1 --advanced" --source-map=../css/theme.css.map
+lessc less/build.less css/theme.css
+
+# Note: Unfortunately the color extractor removes the CSS variables
 css-color-extractor css/theme.css css/theme.css --format=css
+
+# Re-add CSS variables and compress CSS
+cp css/theme.css css/temporary.less
+lessc less/build2.less css/theme.css --clean-css="--s1 --advanced" --source-map=../css/theme.css.map
+rm css/temporary.less
+
+# 1b) DARKENTERPRISE
 cd ../DarkEnterprise
-lessc less/build.less css/theme.css --clean-css="--s1 --advanced" --source-map=../css/theme.css.map
+lessc less/build.less css/theme.css
 css-color-extractor css/theme.css css/theme.css --format=css
+
+cp css/theme.css css/temporary.less
+
+lessc ../DarkHumHub/less/build2.less css/theme.css --clean-css="--s1 --advanced" --source-map=../css/theme.css.map
+rm css/temporary.less
+
 cd ../../
 
-# Update message files
-cd ../../ php yii message/extract-module dark-mode
+# 2) Update message files
+cd ../../
+php yii message/extract-module dark-mode
 cd modules/dark-mode
--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -32,8 +32,8 @@ Changelog
 - Enh: documentation
 
 ## 0.2.0 (6/6/2023)
-- Enh: black top bar
-- Enh: module compatibility (https://github.com/felixhahnweilheim/humhub-dark-mode/issues/1)
+- Theme: black top bar
+- Theme: module compatibility (https://github.com/felixhahnweilheim/humhub-dark-mode/issues/1)
 
 ## 0.1.0 (30/5/2023)
 - first version
