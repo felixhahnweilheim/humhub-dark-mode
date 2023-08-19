15dc4d9bcff2548920b135eca5970e84aeeb2e49
felixhahnweilheim <info@hahn-felix.de>
2023-08-19 12:23:28 +0200

    update message files

--- messages/de/admin.php
+++ messages/de/admin.php
@@ -5,7 +5,5 @@ return [
     'Current base theme: ' => 'Aktuelles Basis-Theme: ',
     'Dark Mode' => 'Dunkler Modus',
     'Dark Theme' => 'Dunkles Theme',
-    'Recommended dark theme: ' => 'Empfohlenes dunkles Theme: ',
     'The stylesheet of the selected theme will be used for the dark mode.' => 'Das Stylesheet des ausgewählten Themes wirdd für den dunklen Modus verwendet.',
-    'Unfortunately we don\'t have a recommended dark theme for your theme.' => 'Leider haben wir kein empfohlenes dunkles Theme für Dein Theme.',
 ];
--- messages/uz/admin.php
+++ messages/uz/admin.php
@@ -5,7 +5,5 @@ return [
     'Current base theme: ' => '',
     'Dark Mode' => '',
     'Dark Theme' => '',
-    'Recommended dark theme: ' => '',
     'The stylesheet of the selected theme will be used for the dark mode.' => '',
-    'Unfortunately we don\'t have a recommended dark theme for your theme.' => '',
 ];
