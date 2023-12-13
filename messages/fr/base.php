a6bc98b46a45715150cfbce6f6806b7bd94361bd
Julien Beauvais <julienth37@users.noreply.github.com>
2023-12-13 18:45:31 +0100

    base.php Fr translation

--- messages/fr/base.php
+++ messages/fr/base.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'Choose "Follow system" to automatically switch between light and dark mode according to your browser or system preferences.' => '',
-    'Dark' => '',
-    'Dark Mode' => '',
-    'Follow system (Default)' => '',
-    'Light' => '',
-    'Theme preferences' => '',
+    'Choose "Follow system" to automatically switch between light and dark mode according to your browser or system preferences.' => 'Choisissez "Suivre le système" pour basculer automatiquement entre les modes clair et sombre en fonction de votre navigateur ou des préférences système.',
+    'Dark' => 'Sombre',
+    'Dark Mode' => 'Mode Sombre',
+    'Follow system (Default)' => 'Suivre le système (par défaut)',
+    'Light' => 'Clair',
+    'Theme preferences' => 'Préférences du thème',
 ];
