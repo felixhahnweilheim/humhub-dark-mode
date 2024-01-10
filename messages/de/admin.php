f2af9f2ff2bef49e14485b739fd838a61f25d9f3
Felix Hahn <info@hahn-felix.de>
2024-01-10 21:13:09 +0100

    Update admin.php

--- messages/de/admin.php
+++ messages/de/admin.php
@@ -11,7 +11,7 @@ return [
     'Thank you!' => 'Vielen Dank!',
     'The Dark Mode module has been developed and is maintained by {name}.' => 'Das Dark Mode-Modul wurde entwickelt und wird gewartet von {name}.',
     'The stylesheet of the selected theme will be used for the dark mode.' => 'Das Stylesheet des ausgewählten Themes wird für den dunklen Modus verwendet.',
-    'Users can set their theme preferences also in Account Settings > General.' => 'Nutzer können die Theme-Einstellug auch in den Account-Einstellungen unter "Allgemein" vornehemen.',
+    'Users can set their theme preferences also in Account Settings > General.' => 'Nutzer können die Theme-Einstellung auch in den Account-Einstellungen unter "Allgemein" vornehemen.',
     'GitHub Repository:' => '',
     'GitHub account' => '',
 ];
