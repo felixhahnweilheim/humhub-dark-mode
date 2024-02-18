536dd0723996e28554d4e05d8df175e756e8ee87
HumHub Translations <hello@humhub.com>
2024-02-18 18:23:49 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/admin.php
+++ messages/de/admin.php
@@ -1,15 +1,14 @@
 <?php
-
-return [
-    '<strong>Dark Mode</strong> module configuration' => '<strong>Dark Mode</strong> Modul-Einstellungen',
-    'Adds a Dark Mode to HumHub' => 'Fügt HumHub einen dunklen Modus hinzu',
-    'Current base theme: ' => 'Aktuelles Basis-Theme: ',
-    'Dark Mode' => 'Dunkler Modus',
-    'Dark Theme' => 'Dunkles Theme',
-    'Donate' => 'Spenden',
-    'Show Button in Top Bar' => 'Button in oberer Leiste anzeigen',
-    'The stylesheet of the selected theme will be used for the dark mode.' => 'Das Stylesheet des ausgewählten Themes wird für den dunklen Modus verwendet.',
-    'Users can set their theme preferences also in Account Settings > General.' => 'Nutzer können die Theme-Einstellung auch in den Account-Einstellungen unter "Allgemein" vornehemen.',
-    'GitHub Repository' => '',
-    'Guide for Administrators' => '',
-];
+return array (
+  '<strong>Dark Mode</strong> module configuration' => '<strong>Dark Mode</strong> Modul-Einstellungen',
+  'Adds a Dark Mode to HumHub' => 'Fügt HumHub einen dunklen Modus hinzu',
+  'Current base theme: ' => 'Aktuelles Basis-Theme:',
+  'Dark Mode' => 'Dunkler Modus',
+  'Dark Theme' => 'Dunkles Theme',
+  'Donate' => 'Spenden',
+  'GitHub Repository' => 'GitHub Repository',
+  'Guide for Administrators' => 'Guide für Administratoren',
+  'Show Button in Top Bar' => 'Button in oberer Leiste anzeigen',
+  'The stylesheet of the selected theme will be used for the dark mode.' => 'Das Stylesheet des ausgewählten Themes wird für den dunklen Modus verwendet.',
+  'Users can set their theme preferences also in Account Settings > General.' => 'Nutzer können die Theme-Einstellung auch in den Account-Einstellungen unter "Allgemein" vornehemen.',
+);
