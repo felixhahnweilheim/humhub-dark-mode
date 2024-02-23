198f671834b6dfc5814e76aa971c211e2f3d9ace
HumHub Translations <hello@humhub.com>
2024-02-23 19:29:07 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/admin.php
+++ messages/ja/admin.php
@@ -1,15 +1,14 @@
 <?php
-
-return [
-    '<strong>Dark Mode</strong> module configuration' => '',
-    'Adds a Dark Mode to HumHub' => '',
-    'Current base theme: ' => '',
-    'Dark Mode' => '',
-    'Dark Theme' => '',
-    'Donate' => '',
-    'GitHub Repository' => '',
-    'Guide for Administrators' => '',
-    'Show Button in Top Bar' => '',
-    'The stylesheet of the selected theme will be used for the dark mode.' => '',
-    'Users can set their theme preferences also in Account Settings > General.' => '',
-];
+return array (
+  '<strong>Dark Mode</strong> module configuration' => '<strong>ダークモード</strong> モジュール構成',
+  'Adds a Dark Mode to HumHub' => 'Humhubにダークモードを追加します',
+  'Current base theme: ' => '現在のベーステーマ：',
+  'Dark Mode' => 'ダークモード',
+  'Dark Theme' => 'ダークテーマ',
+  'Donate' => '寄付',
+  'GitHub Repository' => 'Githubリポジトリ',
+  'Guide for Administrators' => '管理者向けガイド',
+  'Show Button in Top Bar' => 'トップバーの表示ボタンを表示します',
+  'The stylesheet of the selected theme will be used for the dark mode.' => '選択したテーマのスタイルシートは、ダークモードに使用されます',
+  'Users can set their theme preferences also in Account Settings > General.' => 'ユーザーは、アカウント設定&gt;一般にテーマの設定を設定できます',
+);
--- messages/nl/admin.php
+++ messages/nl/admin.php
@@ -1,15 +1,14 @@
 <?php
-
-return [
-    '<strong>Dark Mode</strong> module configuration' => '',
-    'Adds a Dark Mode to HumHub' => '',
-    'Current base theme: ' => '',
-    'Dark Mode' => '',
-    'Dark Theme' => '',
-    'Donate' => '',
-    'GitHub Repository' => '',
-    'Guide for Administrators' => '',
-    'Show Button in Top Bar' => '',
-    'The stylesheet of the selected theme will be used for the dark mode.' => '',
-    'Users can set their theme preferences also in Account Settings > General.' => '',
-];
+return array (
+  '<strong>Dark Mode</strong> module configuration' => 'Moduleconfiguratie <strong>Donkere modus</strong>',
+  'Adds a Dark Mode to HumHub' => 'Voegt een donkere modus toe aan HumHub',
+  'Current base theme: ' => 'Huidig basisthema:',
+  'Dark Mode' => 'Donker modus',
+  'Dark Theme' => 'Donkerthema',
+  'Donate' => 'Doneer',
+  'GitHub Repository' => 'GitHub Repository',
+  'Guide for Administrators' => 'Handleiding voor beheerders',
+  'Show Button in Top Bar' => 'Knop weergeven in bovenste balk',
+  'The stylesheet of the selected theme will be used for the dark mode.' => 'Voor de donkere modus wordt het stylesheet van het geselecteerde thema gebruikt.',
+  'Users can set their theme preferences also in Account Settings > General.' => 'Gebruikers kunnen hun themavoorkeuren ook instellen in Accountinstellingen &gt; Algemeen.',
+);
