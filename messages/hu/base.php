d305261bce93bcbf560e6c0cc1411e8e74b49c89
HumHub Translations <hello@humhub.com>
2024-03-14 15:29:44 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Choose "Follow system" to automatically switch between light and dark mode according to your browser or system preferences.' => '',
-    'Dark' => '',
-    'Dark Mode' => '',
-    'Follow system (Default)' => '',
-    'Light' => '',
-    'Theme preferences' => '',
-];
+return array (
+  'Choose "Follow system" to automatically switch between light and dark mode according to your browser or system preferences.' => 'Válassza a „Rendszer követése” lehetőséget a világos és sötét mód közötti automatikus váltáshoz a böngésző vagy a rendszer beállításai szerint.',
+  'Dark' => 'Sötét',
+  'Dark Mode' => 'Sötét Mód',
+  'Follow system (Default)' => 'Rendszer követése (alapértelmezett)',
+  'Light' => 'Világos',
+  'Theme preferences' => 'A téma beállításai',
+);
