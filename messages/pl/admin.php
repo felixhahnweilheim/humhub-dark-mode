4da7ce85a1edc376e36b366c9f7e135d1b9fcd03
HumHub Translations <hello@humhub.com>
2024-04-20 18:01:19 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/admin.php
+++ messages/pl/admin.php
@@ -1,15 +1,14 @@
 <?php
-
-return [
-    '<strong>Dark Mode</strong> module configuration' => '',
-    'Adds a Dark Mode to HumHub' => '',
-    'Current base theme: ' => '',
-    'Dark Mode' => '',
-    'Dark Theme' => '',
-    'Donate' => '',
-    'GitHub Repository' => '',
-    'Guide for Administrators' => '',
-    'Show Button in Top Bar' => '',
-    'The stylesheet of the selected theme will be used for the dark mode.' => '',
-    'Users can set their theme preferences also in Account Settings > General.' => '',
-];
+return array (
+  '<strong>Dark Mode</strong> module configuration' => '<strong>Tryb nocny</strong> konfiguracja modułu',
+  'Adds a Dark Mode to HumHub' => 'Dodaje tryb nocny do HumHub',
+  'Current base theme: ' => 'Obecny główny wystrój:',
+  'Dark Mode' => 'Tryb nocny',
+  'Dark Theme' => 'Ciemny wystrój',
+  'Donate' => 'Wesprzyj',
+  'GitHub Repository' => 'Repozytorium GitHub',
+  'Guide for Administrators' => 'Podręcznik dla administratorów',
+  'Show Button in Top Bar' => 'Pokaż przycisk w górnej belce',
+  'The stylesheet of the selected theme will be used for the dark mode.' => 'Styl z tego wystroju zostanie wykorzystany na potrzeby trybu nocnego.',
+  'Users can set their theme preferences also in Account Settings > General.' => 'Użytkownicy mogą zmienić wystrój w Ustawienia konta &gt; Ogólne.',
+);
