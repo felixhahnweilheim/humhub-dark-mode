21672c86af2c20653340657b67f71ddca85b2875
HumHub Translations <hello@humhub.com>
2024-07-03 09:46:49 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sk/admin.php
+++ messages/sk/admin.php
@@ -1,15 +1,14 @@
 <?php
-
-return [
-    '<strong>Dark Mode</strong> module configuration' => '',
-    'Adds a Dark Mode to HumHub' => '',
-    'Current base theme: ' => '',
-    'Dark Mode' => '',
-    'Dark Theme' => '',
-    'Donate' => '',
-    'GitHub Repository' => '',
-    'Guide for Administrators' => '',
-    'Show Button in Top Bar' => '',
-    'The stylesheet of the selected theme will be used for the dark mode.' => '',
-    'Users can set their theme preferences also in Account Settings > General.' => '',
-];
+return array (
+  '<strong>Dark Mode</strong> module configuration' => 'Konfigurácia modulu <strong>Tmavý režim</strong>',
+  'Adds a Dark Mode to HumHub' => 'Pridáva do HumHub tmavý režim',
+  'Current base theme: ' => 'Aktuálna základná téma:',
+  'Dark Mode' => 'Tmavý režim',
+  'Dark Theme' => 'Tmavá téma',
+  'Donate' => 'Darovať',
+  'GitHub Repository' => 'Úložisko GitHub',
+  'Guide for Administrators' => 'Príručka pre správcov',
+  'Show Button in Top Bar' => 'Zobraziť tlačidlo v hornej lište',
+  'The stylesheet of the selected theme will be used for the dark mode.' => 'Pre tmavý režim sa použije šablóna so štýlmi vybranej témy.',
+  'Users can set their theme preferences also in Account Settings > General.' => 'Používatelia si môžu nastaviť predvoľby motívov aj v časti Nastavenia účtu &gt; Všeobecné.',
+);
--- messages/sk/base.php
+++ messages/sk/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Choose "Follow system" to automatically switch between light and dark mode according to your browser or system preferences.' => '',
-    'Dark' => '',
-    'Dark Mode' => '',
-    'Follow system (Default)' => '',
-    'Light' => '',
-    'Theme preferences' => '',
-];
+return array (
+  'Choose "Follow system" to automatically switch between light and dark mode according to your browser or system preferences.' => 'Zvoľte "Sledovať systém" na automatické prepínanie medzi svetlým a tmavým režimom podľa vášho prehliadača alebo preferencií systému.',
+  'Dark' => 'Tmavý',
+  'Dark Mode' => 'Tmavý Režim',
+  'Follow system (Default)' => 'Sledovať systém (predvolené)',
+  'Light' => 'Svetlý',
+  'Theme preferences' => 'Predvoľby témy',
+);
