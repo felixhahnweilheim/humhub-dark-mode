3d99647b6a68fbc11b23aa9f2890cd5bf569395f
HumHub Translations <hello@humhub.com>
2024-08-28 14:59:57 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ar/admin.php
+++ messages/ar/admin.php
@@ -1,15 +1,14 @@
 <?php
-
-return [
-    '<strong>Dark Mode</strong> module configuration' => '',
-    'Adds a Dark Mode to HumHub' => '',
-    'Current base theme: ' => '',
-    'Dark Mode' => '',
-    'Dark Theme' => '',
-    'Donate' => '',
-    'GitHub Repository' => '',
-    'Guide for Administrators' => '',
-    'Show Button in Top Bar' => '',
-    'The stylesheet of the selected theme will be used for the dark mode.' => '',
-    'Users can set their theme preferences also in Account Settings > General.' => '',
-];
+return array (
+  '<strong>Dark Mode</strong> module configuration' => 'إعداد موديول <strong>الوضع المظلم</strong>',
+  'Adds a Dark Mode to HumHub' => 'يضيف الوضع المظلم إلى HumHub',
+  'Current base theme: ' => 'الموضوع الأساسي الحالي:',
+  'Dark Mode' => 'الوضع المظلم',
+  'Dark Theme' => 'السمة المظلمة',
+  'Donate' => 'تبرع',
+  'GitHub Repository' => 'مستودع GitHub',
+  'Guide for Administrators' => 'دليل للمسؤولين',
+  'Show Button in Top Bar' => 'عرض الزر في الشريط العلوي',
+  'The stylesheet of the selected theme will be used for the dark mode.' => 'سيتم استخدام ورقة أنماط السمة المحددة للوضع المظلم.',
+  'Users can set their theme preferences also in Account Settings > General.' => 'يمكن للمستخدمين تعيين تفضيلات السمات الخاصة بهم أيضًا في إعدادات الحساب &gt; عام.',
+);
--- messages/ar/base.php
+++ messages/ar/base.php
@@ -1,10 +1,9 @@
 <?php
-
-return [
-    'Choose "Follow system" to automatically switch between light and dark mode according to your browser or system preferences.' => '',
-    'Dark' => '',
-    'Dark Mode' => '',
-    'Follow system (Default)' => '',
-    'Light' => '',
-    'Theme preferences' => '',
-];
+return array (
+  'Choose "Follow system" to automatically switch between light and dark mode according to your browser or system preferences.' => 'اختر "متابعة النظام" للتبديل تلقائيًا بين الوضع الفاتح والمظلم وفقًا لمتصفحك أو تفضيلات النظام.',
+  'Dark' => 'مظلم',
+  'Dark Mode' => 'الوضع المظلم',
+  'Follow system (Default)' => 'متابعة النظام (افتراضي)',
+  'Light' => 'فاتح',
+  'Theme preferences' => 'تفضيلات السمة',
+);
