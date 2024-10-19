75689eb7696e8d16749b6b03e359d290a4054a0a
HumHub Translations <hello@humhub.com>
2024-10-19 21:45:22 +0000

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/es-419/admin.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    '<strong>Dark Mode</strong> module configuration' => '',
+    'Adds a Dark Mode to HumHub' => '',
+    'Current base theme: ' => '',
+    'Dark Mode' => '',
+    'Dark Theme' => '',
+    'Donate' => '',
+    'GitHub Repository' => '',
+    'Guide for Administrators' => '',
+    'Show Button in Top Bar' => '',
+    'The stylesheet of the selected theme will be used for the dark mode.' => '',
+    'Users can set their theme preferences also in Account Settings > General.' => '',
+];
