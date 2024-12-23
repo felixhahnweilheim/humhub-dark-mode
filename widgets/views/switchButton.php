a860ac62474e2f18ffa15781dffc5d54aed9e59b
Felix Hahn <info@hahn-felix.de>
2024-12-23 23:20:53 +0100

    block dark mode button when in user settings form

--- widgets/views/switchButton.php
+++ widgets/views/switchButton.php
@@ -1,7 +1,7 @@
 <?php
 
 ?>
-<div class="btn-group">
+<div class="btn-group dark-mode">
     <a id="dark-mode-button" href="#" data-action-click="ui.modal.load" data-action-click-url="<?= $url ?>">
         <?= $icon ?>
     </a>
