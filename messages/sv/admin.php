3be8d71f185be9f64b8d52df8c1a80826cbcd884
HumHub Translations <hello@humhub.com>
2025-01-13 10:50:59 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sv/admin.php
+++ messages/sv/admin.php
@@ -1,15 +1,14 @@
 <?php
-
 return [
-    '<strong>Dark Mode</strong> module configuration' => '',
-    'Adds a Dark Mode to HumHub' => '',
-    'Current base theme: ' => '',
-    'Dark Mode' => '',
-    'Dark Theme' => '',
-    'Donate' => '',
-    'GitHub Repository' => '',
-    'Guide for Administrators' => '',
-    'Show Button in Top Bar' => '',
-    'The stylesheet of the selected theme will be used for the dark mode.' => '',
-    'Users can set their theme preferences also in Account Settings > General.' => '',
+  '<strong>Dark Mode</strong> module configuration' => '<strong>Mörkt läge</strong>-modulkonfiguration',
+  'Adds a Dark Mode to HumHub' => 'Lägger till ett mörkt läge till HumHub',
+  'Current base theme: ' => 'Nuvarande grundtema:',
+  'Dark Mode' => 'Mörkt läge',
+  'Dark Theme' => 'Mörkt tema',
+  'Donate' => 'Donera',
+  'GitHub Repository' => 'GitHub Repository',
+  'Guide for Administrators' => 'Guide för administratörer',
+  'Show Button in Top Bar' => 'Visa knapp i toppfältet',
+  'The stylesheet of the selected theme will be used for the dark mode.' => 'Stilmallen för det valda temat kommer att användas för mörkt läge.',
+  'Users can set their theme preferences also in Account Settings > General.' => 'Användare kan även ställa in sina temapreferenser i Kontoinställningar &gt; Allmänt.',
 ];
--- messages/sv/base.php
+++ messages/sv/base.php
@@ -1,10 +1,9 @@
 <?php
-
 return [
-    'Choose "Follow system" to automatically switch between light and dark mode according to your browser or system preferences.' => '',
-    'Dark' => '',
-    'Dark Mode' => '',
-    'Follow system (Default)' => '',
-    'Light' => '',
-    'Theme preferences' => '',
+  'Choose "Follow system" to automatically switch between light and dark mode according to your browser or system preferences.' => 'Välj "Följ system" för att automatiskt växla mellan ljust och mörkt läge enligt din webbläsare eller systeminställningar.',
+  'Dark' => 'Mörkt',
+  'Dark Mode' => 'Mörkt läge',
+  'Follow system (Default)' => 'Följ systemet (standard)',
+  'Light' => 'Ljust',
+  'Theme preferences' => 'Preferenser för tema',
 ];
