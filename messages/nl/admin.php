330ffc1f01e813a940446ddd0fa31ffe0af4e087
HumHub Translations <hello@humhub.com>
2025-10-02 22:21:30 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/admin.php
+++ messages/nl/admin.php
@@ -1,9 +1,8 @@
 <?php
-
 return [
-    '<strong>Dark Mode</strong> module configuration' => 'Moduleconfiguratie <strong>Donkere modus</strong>',
-    'Dark Mode' => 'Donker modus',
-    'Show Button in Top Bar' => 'Knop weergeven in bovenste balk',
-    'Users can set their theme preferences also in Account Settings > General.' => 'Gebruikers kunnen hun themavoorkeuren ook instellen in Accountinstellingen &gt; Algemeen.',
-    'Adds a Dark Mode Switcher to HumHub' => '',
+  '<strong>Dark Mode</strong> module configuration' => 'Moduleconfiguratie <strong>Donkere modus</strong>',
+  'Adds a Dark Mode Switcher to HumHub' => 'Voegt een donkere modus-schakelaar toe aan HumHub',
+  'Dark Mode' => 'Donker modus',
+  'Show Button in Top Bar' => 'Knop weergeven in bovenste balk',
+  'Users can set their theme preferences also in Account Settings > General.' => 'Gebruikers kunnen hun themavoorkeuren ook instellen in Accountinstellingen &gt; Algemeen.',
 ];
