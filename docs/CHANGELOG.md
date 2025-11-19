0fc46ca4c9658814e48f43795243389672831fa0
felixhahnweilheim <info@felixwebdesign.de>
2025-11-19 15:50:08 +0100

    fix padding in dark mode button

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,5 +1,8 @@
 Changelog
 =========
+## 2.0.2 (19/11/2025)
+- Fix padding of dark mode button
+
 ## 2.0.1 (06/09/2025)
 - Fix PHP Warning in migration file (use statement with non-compound nam 'Yii' has no effect)
 
--- widgets/views/switchButton.php
+++ widgets/views/switchButton.php
@@ -1,10 +1,13 @@
 <?php
 
 use humhub\widgets\bootstrap\Button;
+use humhub\helpers\Html;
+use humhub\modules\ui\icon\widgets\Icon;
 
 ?>
 <div class="btn-group dark-mode">
-    <?= Button::asLink()
-        ->icon('moon-o')
-        ->action('ui.modal.load', $url) ?>
+    <?= Html::a(Icon::get('moon-o'), '#', [
+        'data-action-click' => 'ui.modal.load',
+        'data-action-click-url' => $url,
+    ]) ?>
 </div>
