94538ce0d186dd5ebfb8a41c2da0b57f84b57fb5
HumHub Translations <hello@humhub.com>
2025-11-23 23:15:46 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/admin.php
+++ messages/pl/admin.php
@@ -1,9 +1,8 @@
 <?php
-
 return [
-    '<strong>Dark Mode</strong> module configuration' => '<strong>Tryb nocny</strong> konfiguracja modułu',
-    'Dark Mode' => 'Tryb nocny',
-    'Show Button in Top Bar' => 'Pokaż przycisk w górnej belce',
-    'Users can set their theme preferences also in Account Settings > General.' => 'Użytkownicy mogą zmienić wystrój w Ustawienia konta &gt; Ogólne.',
-    'Adds a Dark Mode Switcher to HumHub' => '',
+  '<strong>Dark Mode</strong> module configuration' => '<strong>Tryb nocny</strong> konfiguracja modułu',
+  'Adds a Dark Mode Switcher to HumHub' => 'Dodaje do HumHub przełącznik na ciemny wystrój',
+  'Dark Mode' => 'Tryb nocny',
+  'Show Button in Top Bar' => 'Pokaż przycisk w górnej belce',
+  'Users can set their theme preferences also in Account Settings > General.' => 'Użytkownicy mogą zmienić wystrój w Ustawienia konta &gt; Ogólne.',
 ];
